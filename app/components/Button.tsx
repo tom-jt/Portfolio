4d9bd1cf5926fcbb6746ea20d20d0346c7325b6f
Tom <tom.liu.jt@gmail.comm>
2024-12-12 16:58:23 +1100

    Restarted project

---
+++ app/components/Button.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const Button = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('Button clicked')}>Button</button>
+    </div>
+  );
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,43 @@
-import Image from "next/image";
+import React from 'react';
+import styles from './page.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere alias corporis impedit similique et eos. Perspiciatis delectus nisi a id officia soluta nesciunt excepturi corrupti, qui eius aspernatur fugit repellat.</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Ut quae temporibus, neque iure eum esse quibusdam ad commodi quos tempore eius perspiciatis. Dolorum quo libero nulla, error incidunt tenetur mollitia quasi quia placeat cupiditate ratione ipsa eaque numquam.</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Sed voluptate quis libero ab eveniet corrupti ex sit voluptatem tempora molestias eos incidunt quaerat sint dolorem perspiciatis enim itaque, quia quae. Totam sed sunt fugiat obcaecati quidem omnis nulla?</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Cumque, minus odio! Consectetur voluptas porro sed optio dolorem laborum incidunt dignissimos odit ut iusto officiis cum esse est ad labore distinctio asperiores, natus delectus, fugiat, modi minima repellat tempora?</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Eligendi, amet beatae, quidem asperiores vel ullam, itaque dignissimos assumenda nulla eum modi eaque ea quos et quae? Maxime corrupti libero assumenda ea reiciendis quas iure quibusdam quasi explicabo mollitia.</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Dolore neque molestiae culpa, aspernatur soluta numquam consequatur veniam sunt, dicta tempore dolor. Praesentium, quisquam, iusto rerum quos aperiam omnis est provident nisi commodi sint porro quia, expedita velit dolores!</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Dolorem placeat magni atque fugiat provident saepe minus, mollitia cumque natus sed earum veniam voluptas quasi rem quod fuga ducimus modi impedit ipsam consectetur? Quidem corporis quas quasi enim iste?</p>
+      </section>
+      <section className={styles.section}>
+        <h1 className='font-bold text-4xl'>Nice Curves</h1>
+        <p>Nihil modi ea laudantium accusantium blanditiis adipisci officia nulla porro doloribus, praesentium ab autem, explicabo, molestiae obcaecati veniam cumque aut optio earum omnis. Eos, iste? Ipsum aperiam delectus adipisci accusantium.</p>
+      </section>
+    </main>
   );
-}
+};
+
+export default Home;
