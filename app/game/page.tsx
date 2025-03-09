d58cbbeaa89854e0b293f76a0092abcf6d920146
Tom <tom.liu.jt@gmail.comm>
2025-03-09 11:44:45 +1100

    Fixed project frame sizing

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavBar, Page } from '@/app/components/NavBar';
+import style from './page.module.css';
 
 const Home = () => {
   return (
     <main>
       <NavBar active={Page.Game} />
-      <div className='pl-28 bg-teal-600'>
-
+      <div className={style.bgBlack + ' absolute pl-28 dynamicHeader w-full h-full flex items-center justify-center'}>
+        <h1>Coming Soon!</h1>
       </div>
     </main>
   );
