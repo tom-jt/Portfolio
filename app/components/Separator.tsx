913ef7b0a949773b713742abc6b32e48780dafb7
Tom <tom.liu.jt@gmail.com>
2025-06-25 18:29:18 +1000

    Fixed separator colour

--- app/components/Separator.tsx
+++ app/components/Separator.tsx
@@ -1,7 +1,7 @@
 export function Separator() {
   return (
     <main>
-      <div className='bg-teal-500/10 w-full h-4'/>
+      <div className='bg-teal-400/5 w-full h-4'/>
       <div className='bg-teal-700/25 w-full h-4'/>
       <div className='bg-teal-700/50 w-full h-4'/>
       <div className='bg-teal-600/75 w-full h-4'/>
